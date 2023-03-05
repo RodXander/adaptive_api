b518830e8ffbdb7eab9e84111812e0034af803d4
rodxander <oasl.rodxander@gmail.com>
2023-03-05 11:34:06 -0700

    added an example project inside the package.

---
+++ example/lib/adaptive_api.g.dart
@@ -0,0 +1,82 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:flutter/widgets.dart';
+
+// ignore_for_file: unused_element
+
+enum AdaptiveVariant { mobileApp, mobileWeb, desktopApp, desktopWeb }
+
+abstract class AdaptiveBuilds {
+  Widget _buildMobileApp(BuildContext context);
+  Widget _buildMobileWeb(BuildContext context);
+  Widget _buildDesktopApp(BuildContext context);
+  Widget _buildDesktopWeb(BuildContext context);
+}
+
+abstract class AdaptiveStatelessWidget extends StatelessWidget
+    implements AdaptiveBuilds {
+  const AdaptiveStatelessWidget.mobileApp({super.key})
+      : variant = AdaptiveVariant.mobileApp;
+
+  const AdaptiveStatelessWidget.mobileWeb({super.key})
+      : variant = AdaptiveVariant.mobileWeb;
+
+  const AdaptiveStatelessWidget.desktopApp({super.key})
+      : variant = AdaptiveVariant.desktopApp;
+
+  const AdaptiveStatelessWidget.desktopWeb({super.key})
+      : variant = AdaptiveVariant.desktopWeb;
+
+  final AdaptiveVariant variant;
+
+  @override
+  Widget build(BuildContext context) {
+    switch (variant) {
+      case AdaptiveVariant.mobileApp:
+        return _buildMobileApp(context);
+      case AdaptiveVariant.mobileWeb:
+        return _buildMobileWeb(context);
+      case AdaptiveVariant.desktopApp:
+        return _buildDesktopApp(context);
+      case AdaptiveVariant.desktopWeb:
+        return _buildDesktopWeb(context);
+    }
+  }
+}
+
+abstract class AdaptiveStatefulWidget extends StatefulWidget {
+  const AdaptiveStatefulWidget.mobileApp({super.key})
+      : variant = AdaptiveVariant.mobileApp;
+
+  const AdaptiveStatefulWidget.mobileWeb({super.key})
+      : variant = AdaptiveVariant.mobileWeb;
+
+  const AdaptiveStatefulWidget.desktopApp({super.key})
+      : variant = AdaptiveVariant.desktopApp;
+
+  const AdaptiveStatefulWidget.desktopWeb({super.key})
+      : variant = AdaptiveVariant.desktopWeb;
+
+  final AdaptiveVariant variant;
+
+  @override
+  AdaptiveState<AdaptiveStatefulWidget> createState();
+}
+
+abstract class AdaptiveState<T extends AdaptiveStatefulWidget> extends State<T>
+    with AdaptiveBuilds {
+  @override
+  Widget build(BuildContext context) {
+    switch (widget.variant) {
+      case AdaptiveVariant.mobileApp:
+        return _buildMobileApp(context);
+      case AdaptiveVariant.mobileWeb:
+        return _buildMobileWeb(context);
+      case AdaptiveVariant.desktopApp:
+        return _buildDesktopApp(context);
+      case AdaptiveVariant.desktopWeb:
+        return _buildDesktopWeb(context);
+    }
+  }
+}
+
